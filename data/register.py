03895febea90bdd94191d8eeeb67ca33a656ea72
programlamb <93641400+programlamb@users.noreply.github.com>
2022-03-26 13:29:17 +0700

    Add register и users and update requirements

--- data/register.py
+++ data/register.py
@@ -1,2 +1,14 @@
-class RegisterForm:
-    pass
+"""Модель для регистрационной формы"""
+
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.fields.simple import EmailField
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Войти')
--- data/users.py
+++ data/users.py
@@ -1,2 +1,33 @@
-class User:
-    pass
+"""Модель для работы с SQL-таблицей users"""
+
+import sqlalchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    """Работа с информацией о пользователях"""
+
+    # Задаём имя таблицы:
+    __tablename__ = 'users'
+
+    # Задаём столбцы таблицы users:
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    # Хранить пароль в открытом виде нельзя, поэтому во Flask есть инструменты,
+    # которые позволяют создавать хеши паролей. При вводе пароля происходит его проверка
+    # на соответствие значению хеша, хранимого в базе данных.
+
+    def set_password(self, password):
+        """Создание хеша пароля"""
+        # Функция используется при регистрации пользователя.
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Проверка пароля"""
+        # Смотрим, соответствует ли пароль значению хеша на сервере:
+        return check_password_hash(self.hashed_password, password)
